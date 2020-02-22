0e4e5bd5fb03d4211185dd88cef21105478dfc67
Afief Afian <afief@skyshi.io>
2020-02-22 16:32:00 +0700

    Add id setter getter method

--- src/main/java/com/afiefafian/book/domain/Book.java
+++ src/main/java/com/afiefafian/book/domain/Book.java
@@ -26,6 +26,14 @@ public class Book {
         this.year = year;
     }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
